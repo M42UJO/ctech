eb0cfca5253eac804fd271c095e6e6edaf8cdde3
M42UJO <khamso0092@gmail.com>
2024-09-15 18:41:11 +0700

    auto cur

--- C_TECH/Current_address.php
+++ C_TECH/Current_address.php
@@ -30,28 +30,23 @@ try {
             "postal_code" => "",
         ];
     }
-
-
 } catch (PDOException $e) {
     echo "Error: " . htmlspecialchars($e->getMessage());
     exit();
 }
 ?>
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="th">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>C-TECH</title>
+    <link rel="stylesheet" href="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/JQL.min.js"></script>
+    <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/typeahead.bundle.js"></script>
+    <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -59,20 +54,16 @@ try {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&family=IBM+Plex+Sans+Thai+Looped:wght@100;200;300;400;500;600;700&family=Itim&family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Mitr:wght@200;300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Thai:wght@100..900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-
 </head>
 <style>
+    .btn:hover {
+        background-position: right center;
 
+    }
 
-.btn:hover {
-    background-position: right center;
-    
-}
-
-.btn-1 {
-    background-image: linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%);
-}
-
+    .btn-1 {
+        background-image: linear-gradient(to right, #f6d365 0%, #fda085 51%, #f6d365 100%);
+    }
 </style>
 
 <body>
@@ -80,8 +71,8 @@ try {
     require_once("nav.php");
     ?>
 
+    
     <div class="container mt-5">
-
         <section class="step-wizard">
             <ul class="step-wizard-list">
                 <li class="step-wizard-item">
@@ -108,14 +99,9 @@ try {
                     <span class="progress-count">6</span>
                     <span class="progress-label">ข้อมูลผู้ปกครอง</span>
                 </li>
-                
+
             </ul>
         </section>
-
-
-
-
-
 
 
         <div class="panel panel-default mb-5">
@@ -127,46 +113,46 @@ try {
                     <div class="panel-heading">ที่อยู่ปัจจุบัน</div>
                     <div class="col-md-2">
                         <label class="form-label">บ้านเลขที่ <span class="required">**</span></label>
-                        <input class="form-control " type="text " placeholder="บ้านเลขที่ " name="house_number" value="<?php echo $Data_view["house_number"];?>" required>
+                        <input class="form-control " type="text " placeholder="บ้านเลขที่ " name="house_number" value="<?php echo $Data_view["house_number"]; ?>" required>
                     </div>
                     <div class="col-md-1">
                         <label class="form-label">หมู่ <span class="required">**</span></label>
-                        <input class="form-control " type="text " placeholder="หมู่ " name="village" value="<?php echo $Data_view["village"];?>" required>
+                        <input class="form-control " type="text " placeholder="หมู่ " name="village" value="<?php echo $Data_view["village"]; ?>" required>
                     </div>
                     <div class="col-md-3">
                         <label class="form-label">ซอย </label>
-                        <input class="form-control " type="text " placeholder="ซอย " name="lane" value="<?php echo $Data_view["lane"];?>">
+                        <input class="form-control " type="text " placeholder="ซอย " name="lane" value="<?php echo $Data_view["lane"]; ?>">
                     </div>
                     <div class="col-md-3">
                         <label class="form-label">ถนน </label>
-                        <input class="form-control " type="text " placeholder="ถนน " name="road" value="<?php echo $Data_view["road"];?>">
+                        <input class="form-control " type="text " placeholder="ถนน " name="road" value="<?php echo $Data_view["road"]; ?>">
                     </div>
                     <div class="col-md-3">
                         <label class="form-label">ตำบล <span class="required">**</span></label>
-                        <input type="text " class="form-control " placeholder="ตำบล " name="sub_district" value="<?php echo $Data_view["sub_district"];?>" required>
+                        <input type="text " class="form-control " placeholder="ตำบล " id="district" name="sub_district" value="<?php echo $Data_view["sub_district"]; ?>" required>
                     </div>
                     <div class="col-md-4">
                         <label class="form-label">อำเภอ <span class="required">**</span></label>
-                        <input type="text " class="form-control " placeholder="อำเภอ " name="district" value="<?php echo $Data_view["district"];?>" required>
+                        <input type="text " class="form-control " placeholder="อำเภอ " id="amphoe" name="district" value="<?php echo $Data_view["district"]; ?>" required>
                     </div>
                     <div class="col-md-4">
                         <label class="form-label">จังหวัด <span class="required">**</span></label>
-                        <input type="text " class="form-control " placeholder="จังหวัด " name="province" value="<?php echo $Data_view["province"];?>" required>
+                        <input type="text " class="form-control " placeholder="จังหวัด " id="province" name="province" value="<?php echo $Data_view["province"]; ?>" required>
                     </div>
                     <div class="col-md-4">
                         <label class="form-label">รหัสไปรษณีย์ <span class="required">** ตัวเลขเท่านั้น</span></label>
-                        <input type="text " class="form-control " placeholder="รหัสไปรษณีย์ " name="postal_code" maxlength="5" value="<?php echo $Data_view["postal_code"];?>" required>
+                        <input type="text " class="form-control " placeholder="รหัสไปรษณีย์ " id="zipcode" name="postal_code" maxlength="5" value="<?php echo $Data_view["postal_code"]; ?>" required>
                     </div>
                     <div class="col-md-2">
                         <a href="Personal_info.php" type="button" class="btn  w-100 py-2 btn-1">
-                        <i class="fa-solid fa-angles-left"></i> ย้อนกลับ
+                            <i class="fa-solid fa-angles-left"></i> ย้อนกลับ
                         </a>
                     </div>
                     <div class="col-md-8">
                     </div>
                     <div class="col-md-2">
                         <button type="submit" name="submit" class="btn  w-100 py-2 btn-1">ถัดไป
-                        <i class="fa-solid fa-angles-right"></i>
+                            <i class="fa-solid fa-angles-right"></i>
                         </button>
 
                     </div>
@@ -175,15 +161,23 @@ try {
 
             </div>
         </div>
+
     </div>
     <?php
     require_once("footer.php");
     ?>
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js " integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL " crossorigin="anonymous "></script>
-    <!-- <script src="script.js"></script> -->
-     </body>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $.Thailand({
+                $district: $('#district'), // input ของตำบล
+                $amphoe: $('#amphoe'), // input ของอำเภอ
+                $province: $('#province'), // input ของจังหวัด
+                $zipcode: $('#zipcode'), // input ของรหัสไปรษณีย์
+            });
+        });
+    </script>
+</body>
 
 </html>
