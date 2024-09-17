af8fa983dfd4dae8439ebdb5e2d8b3e95c7e7aa6
M42UJO <khamso0092@gmail.com>
2024-09-17 16:17:44 +0700

    auto comp2

--- C_TECH/Mother_info.php
+++ C_TECH/Mother_info.php
@@ -54,15 +54,18 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>C-TECH</title>
+    <link rel="stylesheet" href="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/JQL.min.js"></script>
+    <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/typeahead.bundle.js"></script>
+    <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&family=IBM+Plex+Sans+Thai+Looped:wght@100;200;300;400;500;600;700&family=Itim&family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Mitr:wght@200;300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Thai:wght@100..900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&family=IBM+Plex+Sans+Thai+Looped:wght@100;200;300;400;500;600;700&family=Itim&family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Mitr:wght@200;300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Serif+Thai:wght@100..900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-
 </head>
 <style>
     .btn {
@@ -177,19 +180,19 @@ try {
                     </div>
                     <div class="col-md-3">
                         <label for="first-name" class="form-label">ตำบล <span class="required">**</span></label>
-                        <input type="text " class="form-control " placeholder="ตำบล " name="mother_sub_district" value="<?php echo $Data_view["mother_sub_district"];?>" required=" ">
+                        <input type="text " class="form-control " placeholder="ตำบล " id="district" name="mother_sub_district" value="<?php echo $Data_view["mother_sub_district"];?>" required=" ">
                     </div>
                     <div class="col-md-3">
                         <label for="last-name" class="form-label">อำเภอ <span class="required">**</span></label>
-                        <input type="text " class="form-control " placeholder="อำเภอ " name="mother_district" value="<?php echo $Data_view["mother_district"];?>" required=" ">
+                        <input type="text " class="form-control " placeholder="อำเภอ " id="amphoe" name="mother_district" value="<?php echo $Data_view["mother_district"];?>" required=" ">
                     </div>
                     <div class="col-md-3">
                         <label for="full-name-eng" class="form-label">จังหวัด <span class="required">**</span></label>
-                        <input type="text " class="form-control " placeholder="จังหวัด " name="mother_province" value="<?php echo $Data_view["mother_province"];?>" required=" ">
+                        <input type="text " class="form-control " placeholder="จังหวัด " id="province" name="mother_province" value="<?php echo $Data_view["mother_province"];?>" required=" ">
                     </div>
                     <div class="col-md-3">
                         <label for="id-number" class="form-label">รหัสไปรษณีย์ <span class="required">** ตัวเลขเท่านั้น</span></label>
-                        <input type="text " class="form-control " placeholder="รหัสไปรษณีย์ " name="mother_postal_code" value="<?php echo $Data_view["mother_postal_code"];?>" maxlength="5 " required=" ">
+                        <input type="text " class="form-control " placeholder="รหัสไปรษณีย์ " id="zipcode" name="mother_postal_code" value="<?php echo $Data_view["mother_postal_code"];?>" maxlength="5 " required=" ">
                     </div>
                     <div class="col-md-3">
                         <label for="phone" class="form-label">เบอร์โทรมารดา<span class="required">**</span></label>
@@ -222,6 +225,16 @@ try {
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js " integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL " crossorigin="anonymous "></script>
     <script src="script.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $.Thailand({
+                $district: $('#district'), // input ของตำบล
+                $amphoe: $('#amphoe'), // input ของอำเภอ
+                $province: $('#province'), // input ของจังหวัด
+                $zipcode: $('#zipcode'), // input ของรหัสไปรษณีย์
+            });
+        });
+    </script>
     
 </body>
 
